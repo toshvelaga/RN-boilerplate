14fc7eb038250b82860f92dd0acd6dfaf8e36633
Tosh Velaga <toshvelaga@gmail.com>
2020-09-20 16:27:45 -0700

    folder structure

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,25 @@
+import { combineReducers } from "redux";
+// import emailReducer from "./emailReducer";
+// import passwordReducer from "./passwordReducer";
+// import fullNameReducer from "./fullNameReducer";
+// import usernameReducer from "./usernameReducer";
+// import pickerReducer from "./pickerReducer";
+// import recordBioReducer from "./recordBioReducer";
+// import websiteReducer from "./websiteReducer";
+// import imageReducer from "./imageReducer";
+// import descriptionReducer from "./descriptionReducer";
+
+const rootReducer = combineReducers({
+	// descriptionReducer,
+	// emailReducer,
+	// fullNameReducer,
+	// imageReducer,
+	// passwordReducer,
+	// pickerReducer,
+	// recordBioReducer,
+	// usernameReducer,
+	// websiteReducer,
+	// imageReducer,
+});
+
+export default rootReducer;
