87be569815bc377583a16e990f42768ae748c606
Tosh Velaga <toshvelaga@gmail.com>
2020-09-20 16:10:09 -0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import TabNav from "./src/components/TabNav/TabNav";
+import LoadingScreen from "./src/screens/LoadingScreen/LoadingScreen";
+import firebase from "./src/firebase/fbConfig";
+import StackNav from "./src/components/StackNav/StackNav";
+
+// Store imports react-redux
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./src/store/reducers/rootReducer";
+
+const store = createStore(rootReducer);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [user, setUser] = useState(null);
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged((user) => {
+			setUser(user);
+		});
+	});
+	return (
+		<>
+			<Provider store={store}>
+				<NavigationContainer>
+					<StatusBar style="light" />
+					{user ? <TabNav /> : <StackNav />}
+				</NavigationContainer>
+			</Provider>
+		</>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
